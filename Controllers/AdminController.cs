220c16460f18134bee9491a289a61cb1ca890032
Ersnkrkaya <Ersnkrkaya@github.com>
2021-05-09 17:01:42 +0300

    Add project files.

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+namespace TravelTripProje.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        Context c = new Context();
+        [Authorize]
+        public ActionResult Index()
+        {
+            var degerler = c.Blogs.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniBlog()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniBlog(Blog p)
+        {
+            c.Blogs.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Sil(int id)
+        {
+           var deger = c.Blogs.Find(id);
+            c.Blogs.Remove(deger);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult BlogGetir(int id)
+        {
+            var deger = c.Blogs.Find(id);
+            return View("BlogGetir", deger);
+        }
+        public ActionResult BlogGuncelle(Blog b)
+        {
+            var deger = c.Blogs.Find(b.ID);
+            deger.Baslik = b.Baslik;
+            deger.Aciklama = b.Aciklama;
+            deger.BlogImage = b.BlogImage;
+            deger.Tarih = b.Tarih;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult YorumListesi()
+        {
+            var yorumlar = c.Yorumlars.ToList();
+            return View(yorumlar);
+        }
+        public ActionResult YorumSil(int id)
+        {
+            var deger = c.Yorumlars.Find(id);
+            c.Yorumlars.Remove(deger);
+            c.SaveChanges();
+            return RedirectToAction("YorumListesi");
+        }
+    }
+}
---
+++ Models/Siniflar/Blog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TravelTripProje.Models.Siniflar
+{
+    public class Blog
+    {
+        public int ID { get; set; }
+        public string Baslik { get; set; }
+        public DateTime Tarih { get; set; }
+        public string Aciklama { get; set; }
+        public string BlogImage { get; set; }
+        public ICollection<Yorumlar> yorumlars { get; set; }
+
+    }
+}
